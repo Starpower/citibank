b223bc72fc2eaf669f92636bc43e648297f6228b
Dheeravathshiva <shiva.dheeravath55@gmail.com>
2024-11-15 05:05:39 +0000

    new code updated in dev.env

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-:w
-
-adfgsdfgadf
-qthis file content of deployment procesADSFSdfss
+this file is content of deploy.sh info
+practiceing on the git merge for the handson
+123456789101112131415161718192021222324252627282930
